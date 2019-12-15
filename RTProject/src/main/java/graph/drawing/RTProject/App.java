b6196e99c7ce6e515913a25f7236b8fca9890885
niklasCarstensen <creeper99.mc@gmail.com>
2019-12-16 00:18:45 +0100

    Init

---
+++ RTProject/src/main/java/graph/drawing/RTProject/App.java
@@ -0,0 +1,13 @@
+package graph.drawing.RTProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
